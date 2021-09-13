23278902f4f428c21d4f6108ce0f796ccd7f12d0
Priscila <priscilassouzap@gmail.com>
2021-09-13 19:01:16 -0300

    Construindo um sistema de controle de ponto e acesso com Spring Boot.

---
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -0,0 +1,48 @@
+package com.dio.live.controller;
+
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.save(jornadaTrabalho);
+    }
+
+    @GetMapping ("/list")
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaById(@PathVariable("idJornada") Long idJornada) throws Exception {
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new Exception("jornada não encontrada")));
+    }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity deleteJornadaById(@PathVariable("idJornada") Long idJornada) throws Exception {
+        try {
+            jornadaService.deleteJornada(idJornada);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
+    }
+
+}
